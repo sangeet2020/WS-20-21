37b49207cf29321732190e9191af297c2b506b41
Sangeet Sagar <15uec053@lnmiit.ac.in>
2021-01-14 03:51:35 +0100

    fucking stupid code working smooth

--- computational-linguistics/ass-4/ibm_model1.py
+++ computational-linguistics/ass-4/ibm_model1.py
@@ -2,47 +2,35 @@
 # -*- coding: utf-8 -*-
 
 # @Author: Sangeet Sagar
-# @Date:   2021-01-10 16:35:59
+# @Date:   2021-01-14 02:03:56
 # @Email:  sasa00001@stud.uni-saarland.de
 # @Organization: Universität des Saarlandes
-# @Last Modified time: 2021-01-11 20:22:19
+# @Last Modified time: 2021-01-14 03:47:56
 
-
-
-
-"""
-<Function of script>
-"""
-
-import os
-import sys
-import argparse
 import math
-import numpy as np
-import operator
-import nltk
 import time
-from sys import getsizeof
-from collections import defaultdict
-from itertools import product
+import os, sys
+import argparse
+import operator
+import numpy as np
+from tqdm import tqdm
+from collections import defaultdict 
 
 
 class IBM_model(object):
     
-    def __init__(self, english_words, foreign_words, sentence_pairs, t_dict={}):
+    def __init__(self, english_words, foreign_words, sentence_pairs, alpha=None, save_model=False):
         self.english_words = english_words
         self.foreign_words = foreign_words
         self.sentence_pairs = sentence_pairs
-        self.t_dict=t_dict
+        self.alpha = alpha
+        self.save_model = save_model
     
     def initial_probablity(self):
-        print("Obtaining inital probablity")
-        self.t_dict = dict((k,1 / (len(self.foreign_words))) for k in product(self.english_words,self.foreign_words))
-        
-        # for word_pair in product(self.english_words,self.foreign_words):
-        #     self.t_dict[word_pair] = 1 / (len(self.foreign_words))
-        #     # if (sys.getsizeof(self.t_dict) > 1):
-        print(sys.getsizeof(self.t_dict)/1e+9,"GB")
+        print("Inital probablity...")
+        self.t_score = np.full((len(self.english_words), 
+                                len(self.foreign_words)), 
+                                1 / (len(self.foreign_words)))
     
     def cond_prob_e_f(self, e_words, f_words): # P(e|f)
         le = len(e_words)
@@ -50,14 +38,16 @@ class IBM_model(object):
         
         epsilon = 1 # Normalization constant
         
-        N_factor = epsilon / ((lf+1)**le)
+        N_factor = epsilon / ((lf)**le)
         product = 1
-        
         for j in range(le):
             inner_sum = 0
-            for i in range(le):
-                # import pdb; pdb.set_trace()
-                inner_sum += self.t_dict[(e_words[j], f_words[i])]
+            for i in range(lf):
+                try:
+                    e_idx, f_idx = (self.e_dict[e_words[j]], self.f_dict[f_words[i]])
+                except IndexError:
+                    import pdb; pdb.set_trace()
+                inner_sum += self.t_score[(e_idx, f_idx)]
             
             product = inner_sum * product
         
@@ -70,195 +60,79 @@ class IBM_model(object):
         for pair in self.sentence_pairs:
             f_words = pair[0]
             e_words = pair[1]
-            # print('english sentence:', e_words, 'foreign sentence:', f_words)
-            
             pp += -math.log(self.cond_prob_e_f(e_words, f_words), 2)
-            # print(self.cond_prob_e_f(e_words, f_words))
-        
-        pp = 2**pp
+        # pp = 2**pp
+        pp = np.power(2,pp)
         return pp
 
     def aligner(self, epochs):
-        
-        print("I am here")
         self.initial_probablity()
-        print("Inital probablity obtained")
-        for _ in range(epochs):
+        print("\nEM algorithm...")
+        
+        self.e_dict = {k: v for v, k in enumerate(self.english_words)}
+        self.f_dict = {k: v for v, k in enumerate(self.foreign_words)}
+        ppxty = []
+        for _ in tqdm(range(epochs), desc="Epochs   "):
             # pp = self.perplexity()
+            # ppxty.append(pp)
             # print("Perplexity: {}".format(pp))
             
             # Initialize
-            total = {}
-            count = {}
-            for f_word in self.foreign_words:
-                total[f_word] = 0
-                for e_word in self.english_words:
-                    count[(e_word, f_word)] = 0
+            count = np.zeros((len(self.english_words), len(self.foreign_words)))
+            total = np.zeros(len(self.foreign_words))
             
-            for sp in self.sentence_pairs:
+            # E- Step
+            for sp in tqdm(self.sentence_pairs, desc="Sentences", total = len(self.sentence_pairs), leave=False):
                 f_words = sp[0]
                 e_words = sp[1]
                 
                 # Compute normalizations
-                s_total = dict()
-                for e in e_words:
-                    s_total[e] = 0
-                    for f in f_words:
-                        s_total[e] += self.t_dict[(e, f)]
-                
+                s_total = np.zeros(len(e_words))
+                for eid, e in enumerate(e_words):
+                    for _, f in enumerate(f_words):
+                        e_idx, f_idx = (self.e_dict[e], self.f_dict[f])
+                        s_total[eid] += self.t_score[(e_idx, f_idx)]
+                        
                 # Collect counts
-                for e in e_words:
-                    for f in f_words:
-                        count[(e, f)] += self.t_dict[(e, f)] / s_total[e]
-                        total[f] += self.t_dict[(e, f)] / s_total[e]
+                for eid, e in enumerate(e_words):
+                    for _, f in enumerate(f_words):
+                        e_idx, f_idx = (self.e_dict[e], self.f_dict[f])
+                        count[(e_idx, f_idx)] += self.t_score[(e_idx, f_idx)] / s_total[eid]
+                        total[f_idx] += self.t_score[(e_idx, f_idx)] / s_total[eid]
             
-            # Estimate probablities
-            for f_word in self.foreign_words:
-                for e_word in self.english_words:
-                    self.t_dict[(e_word, f_word)] = count[(e_word, f_word)] / total[f_word]
-            print("T-dict:")
-            for i,v in self.t_dict.items():
-                print(i,v)
-            print("##"*20)
-    
+            # Estimate probablities: M-step
+            self.t_score = count / total
+            
+        if self.save_model:
+            print("Saving Model...")
+            if self.alpha:
+                outfile = "trans_prob_alpha_" + str(self.alpha) + "_epochs_" + str(epochs)+".npy"
+            else:
+                outfile = "trans_prob_bs" + "_epochs_" + str(epochs)+".npy"
+            np.save(outfile, self.t_score)
+            print("Model saved as", outfile)
+            print("*"*30)
+        # return ppxty
+            
     def extractor(self):
         # Extracting alignments from probablity scores
-        threshold=0.01
-        alignments = {}
-        for e_word in self.english_words:
-            sig_score = {}
-            for word_pair, score in self.t_dict.items():
-                if word_pair[0] == e_word:
-                    sig_score[word_pair[1]] = score
-            # import pdb; pdb.set_trace()
-            threshold_query = dict((k, v) for k, v in sig_score.items() if v >= threshold)
-            alignments[e_word] = [*threshold_query.keys()]
-            # import pdb; pdb.set_trace()
-        # print(alignments)
-        # print("Prob dict: ", self.t_dict)
+        # self.t_score = np.load('trans_prob_alpha_0.35_epochs_10.npy')
+        
+        e_dict = {v: k for v, k in enumerate(self.english_words)}
+        f_dict = {v: k for v, k in enumerate(self.foreign_words)}
+        alignments = defaultdict(list)
+        
+        for eid, line in enumerate(self.t_score):
+            if self.alpha:
+                top_idx = np.argwhere(line >= self.alpha).T[0]
+            else:
+                top_idx = [np.argmax(line)]
+            for _, fid in enumerate(top_idx): 
+                alignments[e_dict[eid]].append(f_dict[fid])
         return alignments
     
-    
-                    
-
-def my_function(arg_1, arg_2, args):
-    """ purpose of my function """
-
-def tokenize(text):
-    f_tokens = nltk.word_tokenize(text[0].lower())
-    e_tokens = nltk.word_tokenize(text[1].lower())
-    return f_tokens, e_tokens
-
-
-def main():
-    """ main method """
-    args = parse_arguments()
-    # os.makedirs(args.out_dir, exist_ok=True)
-    sentence_pairs = [ 
-        [ ['das', 'Haus'], ['the', 'house'] ], 
-        [ ['das', 'Buch'], ['the', 'book'] ], 
-        [ ['ein', 'Buch'], ['a', 'book'] ]
-    ]
-    
-    # e_file = "jhu-mt-hw/hw2/data/hansards.e"
-    # f_file = "jhu-mt-hw/hw2/data/hansards.f"
-    # e_file = "hansards.e"
-    # f_file = "hansards.f"
-    # sentence_pairs = []
-    # start = time.time()
-    # with open(f_file, 'r') as f_fpw, open(e_file, 'r') as e_fpw:
-    #     file_iter = list(zip(f_fpw, e_fpw))
-    #     for line in file_iter:
-    #         f_tokens, e_tokens = tokenize(line)
-    #         sentence_pairs.append([f_tokens, e_tokens])    
-    # end=  time.time()
-    # print("Total runtime data loading: %.3f s" % (end-start))
-    
-    # sentence_pairs = [ 
-    #     [ ['la', 'maison'], ['the', 'house'] ], 
-    #     [ ['la', 'maison', 'bleu'], ['the', 'blue', 'house'] ], 
-    #     [ ['la', 'fleur'], ['the', 'flower'] ]
-    # ]
-    
-    # import pdb; pdb.set_trace()
-    print("No. of sentence of pairs= ", len(sentence_pairs))
-    print("*"*40)
-    print("Sentence pairs:")
-    for pair in sentence_pairs:
-        print(pair)
-    english_words = []
-    foreign_words = []
-    for pair in sentence_pairs:
-        foregin_pair = pair[0]
-        english_pair = pair[1]
-        for e_word in english_pair:
-            english_words.append(e_word)
-        for f_word in foregin_pair:
-            foreign_words.append(f_word)
-    
-    english_words = sorted(list(set(english_words)), key=lambda s: s.lower()) 
-    foreign_words = sorted(list(set(foreign_words)), key=lambda s: s.lower()) 
-    # print("*"*40)
-    print("Foreign words: ", len(foreign_words))
-    # print("*"*40)
-    print("English words: ", len(english_words))
-    # print("*"*40)
-    ibm_model = IBM_model(english_words, foreign_words, sentence_pairs)
-    start = time.time()
-    ibm_model.aligner(epochs=7)
-    end = time.time()
-    print("Total runtime aligner: %.3f s" % (end-start))
-    
-    start = time.time()
-    alignments = ibm_model.extractor()
-    end = time.time()
-    print("Total runtime extractor: %.3f s" % (end-start))
-    # print(alignments)
-    # for i,v in alignments.items():
-    #     print(i,v)
-    
-    
-    """ Mappings:
-    {'blue': 'bleu', 'flower': 'fleur', 'house': 'maison', 'the': 'la'}
-    """
-    print("*"*40)
-    # print("Alignments: ", alignments)
-    print("*"*40)
-    for sp in sentence_pairs:
-        foreign_sent = sp[0]
-        english_sent = sp[1]
-        for e_word in english_sent:
-            e_idx = english_sent.index(e_word)+1
-            get_f_words = alignments.get(e_word)
-            try:
-                # import pdb; pdb.set_trace()
-                for word in get_f_words:
-                    f_idx = foreign_sent.index(word)+1
-                    sys.stdout.write("%i-%i " % (e_idx,f_idx))
-            except ValueError:
-                f_idx = 0
-                sys.stdout.write("%i-%i " % (e_idx,f_idx))
-            # f_idx = 0
-            # sys.stdout.write("%i-%i " % (e_idx,f_idx))
-            # e_idx = english_sent.index(e_word)+1
-            
-        sys.stdout.write("\n")
-            
+    def _return_index(self, e_word, f_word):
+        # Return the index in the form of tuple (e_idx, f_idx)
+        # so as to append the score in the t_score matrix
         
-    
-
-    
-
-def parse_arguments():
-    """ parse arguments """
-
-    parser = argparse.ArgumentParser(description=__doc__)
-    # parser.add_argument("arg_1", help="describe arg_1")
-    # parser.add_argument("arg_2", help="describe arg_2")
-    # parser.add_argument("-optional_arg", default=default_value, type=int/"", help='optional_arg meant for some purpose')
-    args = parser.parse_args()
-    return args
-    
-if __name__ == "__main__":
-    main()
-
+        return (self.english_words.index(e_word), self.foreign_words.index(f_word))
---
+++ computational-linguistics/ass-4/main.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @Author: Sangeet Sagar
+# @Date:   2021-01-10 16:35:59
+# @Email:  sasa00001@stud.uni-saarland.de
+# @Organization: Universität des Saarlandes
+# @Last Modified time: 2021-01-14 03:48:18
+
+
+"""
+<Function of script>
+"""
+import os
+import time
+import argparse
+import numpy as np
+from collections import defaultdict
+from ibm_model1 import IBM_model
+import matplotlib.pyplot as plt
+
+def data_loader(e_file, f_file, args):
+    """ purpose of my function """
+    #**************** Some Toy Example ****************
+    # sentence_pairs = [ 
+    #     [ ['la', 'maison'], ['the', 'house'] ], 
+    #     [ ['la', 'maison', 'bleu'], ['the', 'blue', 'house'] ], 
+    #     [ ['la', 'fleur'], ['the', 'flower'] ]
+    # ]
+    # sentence_pairs = [ 
+    #     [ ['das', 'Haus'], ['the', 'house'] ], 
+    #     [ ['das', 'Buch'], ['the', 'book'] ], 
+    #     [ ['ein', 'Buch'], ['a', 'book'] ]
+    # ]
+    #************************************************
+    sentence_pairs = [[sentence.strip().split() for sentence in pair] for pair in zip(open(f_file), open(e_file))][:args.num_sents]
+    english_words = []
+    foreign_words = []
+    for pair in sentence_pairs:
+        for e_word in pair[1]:
+            english_words.append(e_word)
+        for f_word in pair[0]:
+            foreign_words.append(f_word)
+    english_words = sorted(list(set(english_words)), key=lambda s: s.lower()) 
+    foreign_words = sorted(list(set(foreign_words)), key=lambda s: s.lower()) 
+    return sentence_pairs, english_words, foreign_words
+
+def perplexity_plot(ppxty, args):
+    # Plot of log base 2 of perplexity
+    plt.figure(figsize=(7,4))
+    plt.plot(range(1, args.epochs+1), np.log10(ppxty), linewidth=4)
+    plt.xlabel('# Iterations')
+    plt.ylabel('Log perplexity')
+    plt.title('Log(2) perplexity vs. iterations')
+    # plt.rcParams.update({'font.size': 12})
+    plt.show()
+
+def main():
+    """ main method """
+    args = parse_arguments()
+    net_start = time.time()
+    os.makedirs(args.out_dir, exist_ok=True)
+    start = time.time()
+    sentence_pairs, english_words, foreign_words = data_loader(args.eng_f, args.foreign_f, args)
+    end =  time.time()
+    print("Total sentence pairs:", len(sentence_pairs))
+    print("*"*30)
+    
+    save_model = args.save_model
+    if args.alpha:
+        alpha = args.alpha
+    else:
+        alpha = None
+    ibm_model = IBM_model(english_words, foreign_words, sentence_pairs, alpha, save_model)
+    
+    start = time.time()
+    # ppxty = ibm_model.aligner(epochs=args.epochs)
+    ibm_model.aligner(epochs=args.epochs)
+    end = time.time()
+    print("\naligner runtime: %.3f s" % (end-start))
+    
+    start = time.time()
+    alignments = ibm_model.extractor()
+    end = time.time()
+    print("extractor runtime : %.3f s" % (end-start))
+
+    outfile = str(args.out_dir) + "/ibm1.a" 
+    myfile = open(outfile, 'w')
+    for sp in sentence_pairs:
+        foreign_sent = sp[0]
+        english_sent = sp[1]
+        for e_word in english_sent:
+            e_idx = english_sent.index(e_word)
+            get_f_words = alignments[e_word]
+            for word in get_f_words:
+                if word in foreign_sent:
+                    f_idx = foreign_sent.index(word)
+                    myfile.write("%i-%i " % (f_idx,e_idx))
+            # if len(get_f_words) == 0:
+            #     f_idx = NULL
+            #     myfile.write("%i-%i " % (f_idx,e_idx))
+        myfile.write("\n")
+    myfile.close()
+    net_end = time.time()
+    print("Total runtime: %.3f s" % (net_end-net_start))
+    # perplexity_plot(ppxty, args)
+            
+        
+def parse_arguments():
+    """ parse arguments """
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("eng_f", help="path to source (eng) file")
+    parser.add_argument("foreign_f", help="path to target (foriegn) file ")
+    parser.add_argument("out_dir", help='output dir to save the obtained alignments')
+    parser.add_argument("-epochs", default=10, type=int, help='number of training epochs for EM')
+    parser.add_argument("-num_sents", default=10**6, type=int, help='number of sentences to train from')
+    parser.add_argument("-alpha", default=0.30, type=float, help='threshold score of translation probability for alignment')
+    parser.add_argument("-save_model", default=False, type=bool, help='save trained model')
+    
+    args = parser.parse_args()
+    return args
+    
+if __name__ == "__main__":
+    main()
+
